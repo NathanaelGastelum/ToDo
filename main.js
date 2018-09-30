6ecaf6a03f3bc170b75907a44f6ad3a553c2b25a
[NathanaelGastelum] <[nathanaelgastelum@gmail.com]>
2018-09-29 18:18:52 -0700

    added message state to main.js

--- main.js
+++ main.js
@@ -7,8 +7,26 @@ function todoList() {
 }
 
 class Hello extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            message: "my friend (from state)!"
+        };
+        this.updateMessage = this.updateMessage.bind(this);
+    }
+
+    updateMessage() {
+        this.setState({
+            message: "my friend (from changed state)"
+        });
+    }
     render() {
-        return <h1>Hello {this.props.message}</h1>;
+        return(
+            <div>
+                <h1>Hello {this.state.message}</h1>
+                <button onClick={this.updateMessage}>Update</button>
+            </div>
+        ) 
     }
 }
 ReactDOM.render(<Hello message="my friend"/>, document.getElementById("root"));
