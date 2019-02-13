08743634ac79154b66c5b044e17a4e4a1e503902
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-02-12 21:28:40 -0800

    first attempt at function to generate ul from object arrays

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-
-var testArray = ['testing', 'testing', 123,];
-
-arrayPrint(testArray);
-
-function arrayPrint(array) {
-    for (var index of array) {
-        document.writeln(index);
-    }
+let today = {
+    uncomplete:['Relax'],
+    complete: ['Work at a coffee shop']
 }
+let thisWeek = {
+    uncomplete:['Unpack', 'Clean your room'],
+    complete: ['Nothing of importance']
+}
+let soon = {
+    uncomplete:['BS Degree', '???', 'Singer 911'],
+    complete: ['Move']
+}
+
+function makeUl(array) {
+    var list = document.createElement('ul');
+
+    for (let i of array) {
+        // Create the list item:
+        let item = document.createElement('li');
+
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i]));
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+    return list;
+}
+document.getElementById('today').appendChild(makeUl(today.uncomplete));
+document.getElementById('this week').appendChild(makeUl(thisWeek.uncomplete));
+document.getElementById('soon').appendChild(makeUl(soon.uncomplete));
