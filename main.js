87e5ea50255ec72d3a9133253d953ca0c7d94b80
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-02-12 16:35:58 -0800

    added designdoc

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-document.write('Hello World');
+
+var testArray = ['testing', 'testing', 123,];
+
+arrayPrint(testArray);
+
+function arrayPrint(array) {
+    for (var index of array) {
+        document.writeln(index);
+    }
+}
