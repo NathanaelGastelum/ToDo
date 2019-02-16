7a62d85f106b1d15b4d3d58dc2081c6998111001
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-02-16 10:14:17 -0800

    implemented for... of loop. (Thanks Max)

--- main.js
+++ main.js
@@ -11,32 +11,28 @@ let soon = {
     complete: ['Move']
 };
 
-// Object.keys(thisWeek).forEach(key => {
-//     console.log(key) // returns the keys in an object
-//     console.log(thisWeek[key])  // returns the appropriate value 
-//  });
+function makeUl(array) {
+    var list = document.createElement('ul');
 
- function makeUl(card) {
-    Object.keys(card).forEach(key => {
-        var list = document.createElement('ul');
-        list.appendChild(document.createTextNode(key));
+    for (let i of array) {
+        // Create the list item:
+        let item = document.createElement('li');
 
-        for (i=0; i<key.length; i++) {
-            // Create the list item:
-            let item = document.createElement('li');
+        // Set its contents:
+        item.appendChild(document.createTextNode(i));
 
-            // Set its contents:
-            item.appendChild(document.createTextNode(key[i]));
+        // Add it to the list:
+        list.appendChild(item);
+    };
 
-            // Add it to the list:
-            list.appendChild(item);
-        };
+    // Finally, return the constructed list:
+    return list;
+};
+document.getElementById('today').appendChild(makeUl(today.uncomplete));
+document.getElementById('today').appendChild(makeUl(today.complete));
 
-        // Finally, return the constructed list:
-        return list;
-    });
- };
+document.getElementById('this week').appendChild(makeUl(thisWeek.uncomplete));
+document.getElementById('this week').appendChild(makeUl(thisWeek.complete));
 
-document.getElementById('today').appendChild(makeUl(today));
-document.getElementById('this week').appendChild(makeUl(thisWeek));
-document.getElementById('soon').appendChild(makeUl(soon));
+document.getElementById('soon').appendChild(makeUl(soon.uncomplete));
+document.getElementById('soon').appendChild(makeUl(soon.complete));
