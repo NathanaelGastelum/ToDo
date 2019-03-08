74024d50806bcae45dcdde41a5b516bad0e17e1f
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-03-07 17:31:50 -0800

    attempted to simplify creating a list from an array

--- main.js
+++ main.js
@@ -1,49 +1,24 @@
-
-let today = {
-    incomplete:['Relax'],
-    complete: ['Work at a coffee shop']
+let list = {
+    complete: ['Work at a coffee shop', 'Clean up app design'],
+    today: ['Shower', 'Buy plane tickets'],
+    thisWeek: [
+        ['Sunday']
+        ['Monday']
+        ['Tuesday']
+        ['Wednesday']
+        ['Thursday']
+        ['Friday', 'Baked Alaska', 'Play Soma']
+        ['Saturday','Bike ride in the morning']], 
+    soon:['Get a web dev job', 'Singer 911']
 };
-let thisWeek = {
-    incomplete:['Unpack', 'Clean your room'],
-    complete: ['Nothing of importance']
-};
-let soon = {
-    incomplete:['BS Degree', '???', 'Singer 911'],
-    complete: ['Move']
-};
 
-function makeList(array) {
-    var list = document.createElement('ul');
 
-    for (let i of array) {
-        // Create the list item:
-        let item = document.createElement('li');
+    var slides = ["slide 1", "slide 2", "slide 3", "slide 4", "slide 5"]
+    var str = '<ul>'
 
-        // Set its contents:
-        item.appendChild(document.createTextNode(i));
+    slides.forEach(function(slide) {
+    str += '<li>'+ slide + '</li>';
+    }); 
 
-        // Add it to the list:
-        list.appendChild(item);
-    };
-
-    // Finally, return the constructed list:
-    return list;
-};
-function addItem() {
-    var value = document.getElementById('item').value;
-    if (value) {
-        today.incomplete.push(value);
-        console.log(today.incomplete);
-    }
-}
-
-document.getElementById('today').appendChild(makeList(today.incomplete)); //TODO: refactor into a createCard function
-document.getElementById('today').appendChild(makeList(today.complete));
-
-document.getElementById('this week').appendChild(makeList(thisWeek.incomplete));
-document.getElementById('this week').appendChild(makeList(thisWeek.complete));
-
-document.getElementById('soon').appendChild(makeList(soon.incomplete));
-document.getElementById('soon').appendChild(makeList(soon.complete));
-
-document.getElementById('add').addEventListener('click', addItem);//TODO: prevent from reseting page
+    str += '</ul>';
+    document.getElementById("today").innerHTML = str;
