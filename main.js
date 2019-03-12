fcd91b511843dff03eb534022cddac8351abfc4e
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-03-11 17:56:42 -0700

    implemented makeList function

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 let list = {
-    complete: ['Work at a coffee shop', 'Clean up app design'],
-    today: ['Shower', 'Buy plane tickets'],
+    complete: ['Work at a coffee shop', 'Clean up app design', 'Buy plane tickets'],
+    today: ['Shower'],
     thisWeek: [
-        ['Sunday']
-        ['Monday']
-        ['Tuesday']
-        ['Wednesday']
-        ['Thursday']
-        ['Friday', 'Baked Alaska', 'Play Soma']
-        ['Saturday','Bike ride in the morning']], 
+        ['Sunday'],
+        ['Monday'],
+        ['Tuesday'],
+        ['Wednesday'],
+        ['Thursday'],
+        ['Friday', 'Baked Alaska', 'Play Soma'],
+        ['Saturday','Bike ride in the morning']],
     soon:['Get a web dev job', 'Singer 911']
 };
 
+function makeList(array,element) {
+    // Reduce will iterate over all the array items and returns a single value.
+    listItems = array.reduce((result, item) => {
+        // Add a string to the result for the current item. This syntax is using template literals.
+        result += `<li>${item}</li>`;
+        
+        // Always return the result in the reduce callback, it will be the value or result in the next iteration.
+        return result;
+    }, ''); // The '' is an empty string, it is the initial value result.
 
-    var slides = ["slide 1", "slide 2", "slide 3", "slide 4", "slide 5"]
-    var str = '<ul>'
+    // Get the element from the DOM in which to display the list, this should be an ul or ol element.
+    resultElement = document.getElementById(element);
 
-    slides.forEach(function(slide) {
-    str += '<li>'+ slide + '</li>';
-    }); 
+    // Set the inner HTML
+    resultElement.innerHTML = listItems;
+};
 
-    str += '</ul>';
-    document.getElementById("today").innerHTML = str;
+makeList(list.today,'today');
+makeList(list.thisWeek,'this week'); //TODO Use the first element of the array to sort the 2D array into subcatagories
+makeList(list.soon,'soon');
+makeList(list.complete,'completed');
