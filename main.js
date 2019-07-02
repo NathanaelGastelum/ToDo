40bb6f65857ae9bb97a3100a3cdc8c3b2be2beb8
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-07-02 13:09:28 -0700

    began implementing thisWeek card

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let list = {
     today: ['Shower'],
-    thisWeek: [],
+    thisWeek: {
+        sunday: [],
+        monday: [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: []
+    },
     soon:['Get a web dev job', 'Singer 911'],
     completed: ['Cuddle','Work at a coffee shop', 'Clean up app design', 'Buy plane tickets', 'Get Baked Alaska']
 };
 
-makeList(list);
+makeList(list, 'todo');
 
 //Creates ul from list object
 //for every item:
@@ -13,46 +21,52 @@ makeList(list);
 //  Create html element 'ul'/'li'
 //  append child item
 
-function makeList (object) {
-    //TODO add new item input to list cards
-    document.getElementById('todo').innerHTML = '';
+function makeList (object, target) {
+    document.getElementById(target).innerHTML = '';
     for (let prop in object){
         let listHeading = document.createElement('ul');
         let heading = document.createTextNode(prop);
+        listHeading.id = prop;
         listHeading.appendChild(heading);
 
-        const targetElement = document.getElementById('todo');
+        const targetElement = document.getElementById(target);
         targetElement.appendChild(listHeading);
 
-        for (let el of object[prop]) {
-            let listItem = document.createElement('li');
-            let item = document.createTextNode(el);
-            listItem.appendChild(item);
-            let button = document.createElement('button');
-            button.id = prop + "-" + el;
-            button.className = 'remove';
-            button.innerText = "X";
-            listItem.appendChild(button);
+        try {
+            for (let el of object[prop]) {
+                let listItem = document.createElement('li');
+                let item = document.createTextNode(el);
+                listItem.appendChild(item);
+                let button = document.createElement('button');
+                button.id = prop + "-" + el;
+                button.className = 'remove';
+                button.innerText = "X";
+                listItem.appendChild(button);
+    
+                listHeading.appendChild(listItem);
+            }
 
-            listHeading.appendChild(listItem);
+            let input = document.createElement('input');
+            input.id = 'input-' + prop;
+            input.type = 'text';
+            input.placeholder = 'New Item'
+            listHeading.appendChild(input);
+
+            let addButton = document.createElement('button');
+            addButton.id = 'button-' + prop;
+            addButton.className = 'add';
+            addButton.innerText = "+";
+            listHeading.appendChild(addButton);
         }
-        let input = document.createElement('input');
-        input.id = 'input-' + prop;
-        input.type = 'text';
-        input.placeholder = 'New Item'
-        listHeading.appendChild(input);
-
-        let addButton = document.createElement('button');
-        addButton.id = 'button-' + prop;
-        addButton.className = 'add';
-        addButton.innerText = "+";
-        listHeading.appendChild(addButton);
+        catch {
+            makeList(object[prop], listHeading.id);
+        }
     }
 }
 
 function addItem(selection, listItem) {
     list[selection].push(listItem);
-    makeList(list);
+    makeList(list, 'todo');
 }
 
 function removeItem(property, item) {
@@ -60,7 +74,7 @@ function removeItem(property, item) {
     const i = prop.indexOf(item);
     if (i > -1) {
         prop.splice(i, 1);
-        makeList(list);
+        makeList(list, 'todo');
     }
 }
 
