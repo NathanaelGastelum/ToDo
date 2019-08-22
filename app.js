339415214743fcbf2e9b1391622c17dc35ef3f33
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-08-21 18:52:12 -0700

    fixed static page serving

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/index');
 
 const app = express();
+
+app.use(express.static('public'))
+
 app.use('/', routes);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('It works!');
+  
 });
 
 module.exports = router;
