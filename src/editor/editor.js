95bfa68f6af238be6e5bb29182583dd1eee06f69
NathanaelGastelum <nathanaelgastelum@gmail.com>
2020-08-09 16:22:27 -0700

    added ability to edit note titles

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -37,8 +37,16 @@ class EditorComponent extends React.Component {
 
         const { classes } = this.props;
 
+        //TODO fix editIcon appearing in the wrong place
         return(
         <div className={classes.editorContainer}>
+            <BorderColorIcon className={classes.editIcon}></BorderColorIcon> 
+            <input
+                className={classes.titleInput}
+                placeholder='Note Title'
+                value={this.state.title ? this.state.title : ''}
+                onChange={(e) => this.updateTitle(e.target.value)}>
+            </input>
             <ReactQuill 
                 value={this.state.text} 
                 onChange={this.updateBody}>
@@ -46,9 +54,13 @@ class EditorComponent extends React.Component {
         </div>);
     }
     updateBody = async (val) => {
-        await this.setState({ text: val })
+        await this.setState({ text: val });
         this.update();
     };
+    updateTitle = async (txt) => {
+        await this.setState({ title: txt });
+        this.update();
+    }
     update = debounce(() => {
         this.props.noteUpdate(this.state.id, {
             title: this.state.title,
